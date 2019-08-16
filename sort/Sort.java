88deb438313de35f7ed3ecb7ff45c9f77fa1c282
Jocoboy <50202665+Jocoboy@users.noreply.github.com>
2019-08-16 18:54:54 +0800

    Update interface class

--- sort/Sort.java
+++ sort/Sort.java
@@ -1,5 +1,8 @@
 package sort;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Common interface for most sorting algorithms.
  */
@@ -12,4 +15,15 @@ public interface Sort {
      * @return a sorted array
      */
     <T extends Comparable<T>> T[] sort(T[] unsorted);
+
+    /**
+     * 
+     * @param <T>
+     * @param unsorted - an list to be sorted
+     * @return a sorted list
+     */
+    @SuppressWarnings("unchecked")
+    default <T extends Comparable<T>> List<T> sort(List<T> unsorted) {
+        return Arrays.asList(sort(unsorted.toArray((T[]) new Comparable[unsorted.size()])));
+    }
 }
