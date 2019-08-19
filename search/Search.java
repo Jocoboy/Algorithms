db0cbb8576fc784c78d4c246c6fd3f7f0c5b3e2d
Jocoboy <50202665+Jocoboy@users.noreply.github.com>
2019-08-19 22:32:11 +0800

    Update interface class

--- search/Search.java
+++ search/Search.java
@@ -11,5 +11,18 @@ public interface Search {
      * @param value - an element to be found
      * @return first found index of the element
      */
-    <T extends Comparable<T>> int find(T[] array, T value);
+    default <T extends Comparable<T>> int find(T[] array, T value){
+        return 0;
+    }
+
+    /**
+     * 
+     * @param <T> - any comparable type
+     * @param array - a 2D array where the element should be found
+     * @param value - an element to be found
+     * @return first found location of the element
+     */
+    default <T extends Comparable<T>> int[] find(T[][] array, T value){
+        return new int[2];
+    }
 }
